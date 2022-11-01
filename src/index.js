b66436b54c67b507ede424557475670d5e8acdf1
NeetuKV <pranee6r@yahoo.com>
2022-11-01 14:09:17 -0700

    updated html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  //calcuate date
+  return `${day} ${hours}: ${minutes}`;
+}
+function displayTemperature(response) {
+  let temeratureElement = document.querySelector("#temperature");
+  temeratureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humudityElement = document.querySelector("#humidity");
+  humudityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate;
+}
+let apiKey = "f61ab93bd3a752952443b3a7d6737cef";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
 
+axios.get(apiUrl).then(displayTemperature);
